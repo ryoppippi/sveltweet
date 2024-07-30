551cc2ff5f1fc964bec55a1242d8101464a92f49
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-30 14:41:09 +0100

    refactor(icons): add verified icons index.ts
    This commit introduces three new icons to the project:
    Verified, VerifiedBusiness, and VerifiedGovernment. These
    icons are exported from the 'icons' directory.

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as Verified } from './Verified.svelte';
+export { default as VerifiedBusiness } from './VerifiedBusiness.svelte';
+export { default as VerifiedGovernment } from './VerifiedGovernment.svelte';
