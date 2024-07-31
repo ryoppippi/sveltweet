50cc84a31110adee866f7e50908a61303b99e8ad
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-31 11:36:44 +0100

    feat(TweetNotFound): Add component
    A new Svelte component, TweetNotFound, has been added. This component
    is displayed when the embedded tweet cannot be found. It has been
    exported in the main index.ts file for use in other parts of the
    application.

---
+++ src/lib/components/TweetNotFound.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+	import TweetContainer from './TweetContainer.svelte';
+</script>
+
+<TweetContainer>
+	<div class='root'>
+		<h3>Tweet not found</h3>
+		<p>The embedded tweet could not be found…</p>
+	</div>
+</TweetContainer>
+
+<style>
+.root {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 0.75rem;
+}
+.root > h3 {
+  font-size: 1.25rem;
+  margin-bottom: 0.5rem;
+}
+</style>
