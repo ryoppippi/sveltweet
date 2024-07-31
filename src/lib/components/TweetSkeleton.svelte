afe28c6a04fb238cecfd0301dfa75af078581454
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-31 12:32:18 +0100

    feat(TweetSkeleton): add  component
    This commit adds a new Svelte component, TweetSkeleton. This component
    provides a skeleton loading screen for tweets. It's included in the main
    export file, making it available for use throughout the application.

---
+++ src/lib/components/TweetSkeleton.svelte
@@ -0,0 +1,53 @@
+<script lang='ts'>
+	import type { HTMLAttributes } from 'svelte/elements';
+	import TweetContainer from './TweetContainer.svelte';
+
+</script>
+{#snippet skeleton({ ...rest }: HTMLAttributes<HTMLSpanElement>)}
+  <!-- svelte-ignore element_invalid_self_closing_tag -->
+  <span class='skeleton' {...rest} />
+{/snippet}
+
+<TweetContainer className='root'>
+	{@render skeleton({ style: 'height: 3rem; margin-bottom: 0.75rem;' })}
+	{@render skeleton({ style: 'height: 6rem; margin: 0.5rem 0;' })}
+
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
+	<div style:border-top='var(--tweet-border)' style:margin='0.5rem 0' />
+
+	{@render skeleton({ style: 'height: 2rem;' })}
+	{@render skeleton(
+		{ style: 'height: 2rem; border-radius: 9999px; margin-top: 0.5rem;' },
+	)}
+</TweetContainer>
+
+<style>
+.root {
+  pointer-events: none;
+  padding-bottom: 0.25rem;
+}
+.skeleton {
+  display: block;
+  width: 100%;
+  border-radius: 5px;
+  background-image: var(--tweet-skeleton-gradient);
+  background-size: 400% 100%;
+  animation: loading 8s ease-in-out infinite;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  .skeleton {
+    animation: none;
+    background-position: 200% 0;
+  }
+}
+
+@keyframes loading {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
+</style>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export { default as SvelteTweet } from './components/Tweet.svelte';
 export { default as SvelteTweetNotFound } from './components/TweetNotFound.svelte';
+export { default as SvelteTweetSkeleton } from './components/TweetSkeleton.svelte';
 export type { EnrichedTweet } from 'react-tweet';
 export type * from 'react-tweet/api';
