a9d6be64816193767314a2c58cce17872cc96d22
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-01 20:50:39 +0100

    feat(svelte.config): disable inspector
    This commit introduces a new configuration for vitePlugin in the
    svelte.config.js file. The inspector property is set to false.

--- svelte.config.js
+++ svelte.config.js
@@ -21,6 +21,10 @@ const config = {
 		runes: true,
 	},
 
+	vitePlugin: {
+		inspector: false,
+	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
