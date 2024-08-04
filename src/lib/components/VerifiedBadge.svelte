f1ca3bbe6ea9920571ce2ab84d6164c1097ee433
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-04 17:48:42 +0100

    feat(VerifiedBadge.svelte): remove svelte:component

--- src/lib/components/VerifiedBadge.svelte
+++ src/lib/components/VerifiedBadge.svelte
@@ -5,7 +5,7 @@
 
 	type Props = { user: TweetUser };
 	type IconComponent = typeof Icons[keyof typeof Icons];
-	type DivSnippetProps = { icon: IconComponent } & HTMLAttributes<HTMLDivElement>;
+	type DivSnippetProps = { Icon: IconComponent } & HTMLAttributes<HTMLDivElement>;
 
 	const { user }: Props = $props();
 
@@ -16,24 +16,24 @@
 
 </script>
 
-{#snippet authorVerifiedDiv({ icon, ...rest }: DivSnippetProps)}
+{#snippet authorVerifiedDiv({ Icon, ...rest }: DivSnippetProps)}
 	<div {...rest} class:authorVerified={true}>
-		<svelte:component this={icon} />
+		<Icon />
 	</div>
 {/snippet}
 
 <!-- @see https://github.com/vercel/react-tweet/blob/3367f07a2177462af1d05d62b1785bb9aa4ab787/packages/react-tweet/src/twitter-theme/verified-badge.tsx#L20-L34 */ -->
 {#if isVerified}
 	{#if isGovernment}
-		{@render authorVerifiedDiv({ icon: Icons.VerifiedGovernment, class: 'verifiedGovernment' })}
+		{@render authorVerifiedDiv({ Icon: Icons.VerifiedGovernment, class: 'verifiedGovernment' })}
 	{:else if isBusiness}
-		{@render authorVerifiedDiv({ icon: Icons.VerifiedBusiness, class: '' })}
+		{@render authorVerifiedDiv({ Icon: Icons.VerifiedBusiness, class: '' })}
 	{:else if !isBlue}
 		<!-- verified but not blue -->
-		{@render authorVerifiedDiv({ icon: Icons.Verified, class: 'verifiedOld' })}
+		{@render authorVerifiedDiv({ Icon: Icons.Verified, class: 'verifiedOld' })}
 	{:else}
 		<!-- normal paid premium user -->
-		{@render authorVerifiedDiv({ icon: Icons.Verified, class: 'verifiedBlue' })}
+		{@render authorVerifiedDiv({ Icon: Icons.Verified, class: 'verifiedBlue' })}
 	{/if}
 {/if}
 
