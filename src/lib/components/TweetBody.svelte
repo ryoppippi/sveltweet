f9d82d1b13672977a1e8fea127e97ffacb6bb316
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 12:19:50 +0100

    feat(TweetBody.svelte) import css using svelte-preprocess-cssmodules

--- src/lib/components/TweetBody.svelte
+++ src/lib/components/TweetBody.svelte
@@ -1,13 +1,14 @@
 <script lang='ts'>
 	import TweetLink from './TweetLink.svelte';
 	import type { EnrichedTweet } from '$rt/utils.js';
+	import s from '$rt_tw/tweet-body.module.css';
 
 	const { tweet }: { tweet: EnrichedTweet } = $props();
 </script>
 
 <!-- eslint-disable svelte/no-at-html-tags -->
 
-<p class='root'>
+<p class={s.root}>
 	{#each tweet.entities as item, i (i)}
 		{#if item.type === 'hashtag' || item.type === 'mention' || item.type === 'url' || item.type === 'symbol'}
 			<TweetLink href={item.href}>{item.text}</TweetLink>
@@ -16,7 +17,3 @@
 		{/if}
 	{/each}
 </p>
-
-<style>
-	@import "$rt_tw/tweet-body.module.css" scoped;
-</style>
