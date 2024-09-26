9f56b13f43a9f85fc32a961769c86787f7b533c5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 12:18:20 +0100

    feat(TweetActionsCopy.svelte) import css using svelte-preprocess-cssmodules

--- src/lib/components/TweetActionsCopy.svelte
+++ src/lib/components/TweetActionsCopy.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import type { EnrichedTweet } from '$rt/utils.js';
+	import s from '$rt_tw/tweet-actions.module.css';
 
 	const { tweet }: { tweet: EnrichedTweet } = $props();
 
@@ -25,17 +26,17 @@
 	});
 </script>
 
-<button class='copy' aria-label='Copy link' onclick={handleCopy} type='button'>
-	<div class='copyIconWrapper'>
+<button class={s.copy} aria-label='Copy link' onclick={handleCopy} type='button'>
+	<div class={s.copyIconWrapper}>
 		{#if copied}
-			<svg class='copyIcon' aria-hidden='true' viewBox='0 0 24 24'>
+			<svg class={s.copyIcon} aria-hidden='true' viewBox='0 0 24 24'>
 				<g>
 					<path d='M9.64 18.952l-5.55-4.861 1.317-1.504 3.951 3.459 8.459-10.948L19.4 6.32 9.64 18.952z'
 					/>
 				</g>
 			</svg>
 		{:else}
-			<svg class='copyIcon' aria-hidden='true' viewBox='0 0 24 24'>
+			<svg class={s.copyIcon} aria-hidden='true' viewBox='0 0 24 24'>
 				<g>
 					<path d='M18.36 5.64c-1.95-1.96-5.11-1.96-7.07 0L9.88 7.05 8.46 5.64l1.42-1.42c2.73-2.73 7.16-2.73 9.9 0 2.73 2.74 2.73 7.17 0 9.9l-1.42 1.42-1.41-1.42 1.41-1.41c1.96-1.96 1.96-5.12 0-7.07zm-2.12 3.53l-7.07 7.07-1.41-1.41 7.07-7.07 1.41 1.41zm-12.02.71l1.42-1.42 1.41 1.42-1.41 1.41c-1.96 1.96-1.96 5.12 0 7.07 1.95 1.96 5.11 1.96 7.07 0l1.41-1.41 1.42 1.41-1.42 1.42c-2.73 2.73-7.16 2.73-9.9 0-2.73-2.74-2.73-7.17 0-9.9z'
 					/>
@@ -43,7 +44,7 @@
 			</svg>
 		{/if}
 	</div>
-	<span class='copyText'>
+	<span class={s.copyText}>
 		{copied ? 'Copied!' : 'Copy link'}
 	</span>
 </button>
