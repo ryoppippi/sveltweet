ec13cf6342f368fd8c4518c9644c17b513d93fd4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-26 12:47:32 +0100

    feat(TweetNotFound.svelte) import css using svelte-preprocess-cssmodules

--- src/lib/components/TweetNotFound.svelte
+++ src/lib/components/TweetNotFound.svelte
@@ -1,14 +1,11 @@
 <script lang='ts'>
 	import TweetContainer from './TweetContainer.svelte';
+	import s from '$rt_tw/tweet-not-found.module.css';
 </script>
 
 <TweetContainer>
-	<div class='root'>
+	<div class={s.root}>
 		<h3>Tweet not found</h3>
 		<p>The embedded tweet could not be found…</p>
 	</div>
 </TweetContainer>
-
-<style>
-	@import "$rt_tw/tweet-not-found.module.css" scoped;
-</style>
