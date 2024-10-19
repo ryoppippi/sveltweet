95971d763a401ba4bf59840d99a0f6b51be1bec8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-19 18:23:51 +0100

    Revert "fix: EmbeddedTweet -> Tweet"
    This reverts commit 729544cbd8fb1451a6b0cf15ee9dfc9ff27f47fb.

--- src/lib/components/Tweet.svelte
+++ src/lib/components/Tweet.svelte
@@ -1,45 +1,10 @@
 <script lang='ts'>
-	import TweetContainer from './TweetContainer.svelte';
-	import TweetHeader from './TweetHeader.svelte';
-	import TweetInReplyTo from './TweetInReply.svelte';
-	import TweetBody from './TweetBody.svelte';
-	import TweetMedia from './TweetMedia.svelte';
-	import TweetInfo from './TweetInfo.svelte';
-	import TweetActions from './TweetActions.svelte';
-	import TweetReplies from './TweetReplies.svelte';
-	import { QuotedTweet } from './quoted';
-	import type { Tweet } from '$rt/api';
-	import { enrichTweet } from '$rt/utils.js';
+	import type { ComponentProps } from 'svelte';
+	import EmbeddedTweet from './EmbeddedTweet.svelte';
 
-	type Props = {
-		tweet: Tweet;
-	};
+	type Props = ComponentProps<EmbeddedTweet>;
 
-	const { tweet }: Props = $props();
-
-	const enrichedTweet = enrichTweet(tweet);
+	const { ...rest }: Props = $props();
 </script>
 
-{#if enrichedTweet != null}
-	<TweetContainer>
-		<TweetHeader tweet={enrichedTweet} />
-
-		{#if enrichedTweet.in_reply_to_status_id_str != null}
-			<TweetInReplyTo tweet={enrichedTweet} />
-		{/if}
-
-		<TweetBody tweet={enrichedTweet} />
-
-		{#if (enrichedTweet.mediaDetails ?? []).length > 0}
-			<TweetMedia tweet={enrichedTweet} />
-		{/if}
-
-		{#if enrichedTweet.quoted_tweet != null}
-			<QuotedTweet tweet={enrichedTweet.quoted_tweet} />
-		{/if}
-
-		<TweetInfo tweet={enrichedTweet} />
-		<TweetActions tweet={enrichedTweet} />
-		<TweetReplies tweet={enrichedTweet} />
-	</TweetContainer>
-{/if}
+<EmbeddedTweet {...rest} />
