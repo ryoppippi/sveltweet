5ccee59f4936cda24f6cc7d443a19503a0b4ced2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-19 23:19:45 +0100

    fix(video): wrap with div

--- src/lib/components/TweetMediaVideo.svelte
+++ src/lib/components/TweetMediaVideo.svelte
@@ -20,86 +20,86 @@
 	const poster = $derived(getMediaUrl(media, 'small'));
 
 	let video: HTMLVideoElement;
-
 </script>
 
-<!-- current does not work @see https://github.com/sveltejs/kit/issues/11057 -->
-<video
-	bind:this={video}
-	class={mediaStyles.image}
-	controls={!playButton}
-	muted
-	onended={() => {
-		ended = true;
-	}}
-	onpause={() => {
-		if (timeout > 0) {
-			clearTimeout(timeout);
-		}
-		timeout = window.setTimeout(() => {
-			if (isPlaying) {
-				isPlaying = false;
-			}
-			timeout = 0;
-		}, 100);
-	}}
-	onplay={() => {
-		if (timeout > 0) {
-			window.clearTimeout(timeout);
-		}
-		if (!isPlaying) {
-			isPlaying = true;
-		}
-		if (ended) {
-			ended = false;
-		}
-	}}
-	{poster}
-	preload='none'
-	tabIndex={playButton ? -1 : 0}
->
-	<source src={mp4Video.url} type={mp4Video.content_type} />
-</video>
-
-{#if playButton}
-	<button
-		class={s.videoButton}
-		aria-label='View video on X'
-		onclick={() => {
-			playButton = false;
-			isPlaying = true;
-			video.play();
-			video.focus();
+<div>
+	<video
+		bind:this={video}
+		class={mediaStyles.image}
+		controls={!playButton}
+		muted
+		onended={() => {
+			ended = true;
 		}}
-		type='button'
+		onpause={() => {
+			if (timeout > 0) {
+				clearTimeout(timeout);
+			}
+			timeout = window.setTimeout(() => {
+				if (isPlaying) {
+					isPlaying = false;
+				}
+				timeout = 0;
+			}, 100);
+		}}
+		onplay={() => {
+			if (timeout > 0) {
+				window.clearTimeout(timeout);
+			}
+			if (!isPlaying) {
+				isPlaying = true;
+			}
+			if (ended) {
+				ended = false;
+			}
+		}}
+		{poster}
+		preload='none'
+		tabIndex={playButton ? -1 : 0}
 	>
-		<svg
-			class={s.videoButtonIcon}
-			aria-hidden='true'
-			viewBox='0 0 24 24'
-		>
-			<g>
-				<path d='M21 12L4 2v20l17-10z' />
-			</g>
-		</svg>
-	</button>
-{/if}
+		<source src={mp4Video.url} type={mp4Video.content_type} />
+	</video>
 
-{#if !isPlaying && !ended}
-	<div class={s.watchOnTwitter}>
-		<a
-			class={s.anchor}
-			href={tweet.url}
-			rel='noopener noreferrer'
-			target='_blank'
+	{#if playButton}
+		<button
+			class={s.videoButton}
+			aria-label='View video on X'
+			onclick={() => {
+				playButton = false;
+				isPlaying = true;
+				video.play();
+				video.focus();
+			}}
+			type='button'
 		>
-			{playButton ? 'Watch on X' : 'Continue watching on X'}
+			<svg
+				class={s.videoButtonIcon}
+				aria-hidden='true'
+				viewBox='0 0 24 24'
+			>
+				<g>
+					<path d='M21 12L4 2v20l17-10z' />
+				</g>
+			</svg>
+		</button>
+	{/if}
+
+	{#if !isPlaying && !ended}
+		<div class={s.watchOnTwitter}>
+			<a
+				class={s.anchor}
+				href={tweet.url}
+				rel='noopener noreferrer'
+				target='_blank'
+			>
+				{playButton ? 'Watch on X' : 'Continue watching on X'}
+			</a>
+		</div>
+	{/if}
+
+	{#if ended}
+		<a class='{s.anchor} {s.viewReplies}' href={tweet.url} rel='noopener noreferrer' target='_blank'>
+			View replies
 		</a>
-	</div>
-{/if}
-
-{#if ended}
-	<a class='{s.anchor} {s.viewReplies}' href={tweet.url} rel='noopener noreferrer' target='_blank'>
-		View replies
-	</a>
-{/if}
+	{/if}
+</div>
